38f1355b3541682559f6b0dc4e7f9a5c0371d7c0
Vincent Barilla <vbarilla@gmail.com>
2016-09-26 16:26:30 -0700

    Use env variables rather than config file

---
+++ db/config.js
@@ -0,0 +1,21 @@
+var Sequelize = require('sequelize');
+
+//AWS DB CONNECTION
+var connectionString = process.env.connectionString;
+
+var connection = new Sequelize(connectionString, {
+  pool: {
+    max: 50,
+    min: 1,
+    idle: 1000000
+  },
+});
+
+// LOCAL DB CONNECTION
+// 1. Run mysqld in terminal tab 1
+// 2. Run mysql in terminal tab 2
+// 3. In terminal tab 2, use command: create database eels1;
+// 4. In terminal tab 2, use command: USE eels1;
+// var connection = new Sequelize('eels1', 'root');
+ 
+module.exports = connection;
