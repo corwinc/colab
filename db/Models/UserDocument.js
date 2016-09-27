8d0b4790b129d90d2d5da6abfe47013d3d279200
Vincent Barilla <vbarilla@gmail.com>
2016-09-27 15:17:18 -0700

    Implement join table for users to documents many to many

---
+++ db/Models/UserDocument.js
@@ -0,0 +1,14 @@
+var Sequelize = require('sequelize');
+var sqlConnection = require('../config.js');
+var User = require('User');
+var Document = require('Document');
+
+UserDocument = sequelize.define('user_document', {
+  status: Sequelize.STRING
+});
+
+User.belongsToMany(Project, { through: UserDocument });
+Document.belongsToMany(User, { through: UserDocument });
+
+User.sync();
+Document.sync();
