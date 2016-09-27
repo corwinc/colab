8cae20b552258d808c0727724cba3c97b3a24127
Vincent Barilla <vbarilla@gmail.com>
2016-09-26 21:58:55 -0700

    Delete public folder from root directory, connect server to static files found in client directory

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/../client'));
 
 app.get('/', function(req, res) {
   res.sendFile('index');
