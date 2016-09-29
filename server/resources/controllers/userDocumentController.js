2ed059970cecde2fe9026e5ddce4eb9de5cf6a4e
Corwin Crownover <corwin.crownover@gmail.com>
2016-09-29 15:55:25 -0700

    Add UserDoc placeholder methods

---
+++ server/resources/controllers/userDocumentController.js
@@ -0,0 +1,12 @@
+var db = require('../../../db/config.js');
+var UserDoc = require('../../../db/Models/UserDocument.js');
+
+// Use case: on doc create
+exports.createUserDoc = function(req, res) {
+  // how does sequelize set up adding these?
+};
+
+exports.getSharedUsers = function(req, res) {
+
+};
+
