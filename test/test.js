8c7470e788f4e150f74414fee5e21680a42b8caa
Corwin Crownover <corwin.crownover@gmail.com>
2016-09-28 17:03:38 -0700

    Add babel-core dependency, cache clean again

--- test/test.js
+++ test/test.js
@@ -1,34 +1,35 @@
-const expect = require('chai').expect;
-const request = require('request');
+// const expect = require('chai').expect;
+// const request = require('request');
 
-describe('Server Tests', () => {
-  // describe('GET request to home page', () => {
-  //   it('Should receive a 200 response code for a GET request to the home page.', (done) => {
-  //     request
-  //       .get('http://127.0.0.1:8000')
-  //       .on('response', (response) => {
-  //         expect(response.statusCode).to.equal(200);
-  //         done();
-  //       });
-  //   });
-  // });
+// describe('Server Tests', () => {
+//   describe('GET request to home page', () => {
+//     it('Should receive a 200 response code for a GET request to the home page.', (done) => {
+//       request
+//         .get('http://127.0.0.1:8000')
+//         .on('response', (response) => {
+//           expect(response.statusCode).to.equal(200);
+//           done();
+//         });
+//     });
+//   });
 
-  // describe('GET request for an unknown route', () => {
-  //   it('Should receive a 404 response code for a GET request to an unknown route.', (done) => {
-  //     request
-  //       .get('http://127.0.0.1:8000/random')
-  //       .on('response', (response) => {
-  //         expect(response.statusCode).to.equal(404);
-  //         done();
-  //       });
-  //   });
-  // });
+//   describe('GET request for an unknown route', () => {
+//     it('Should receive a 404 response code for a GET request to an unknown route.', (done) => {
+//       request
+//         .get('http://127.0.0.1:8000/random')
+//         .on('response', (response) => {
+//           expect(response.statusCode).to.equal(404);
+//           done();
+//         });
+//     });
+//   });
+// });
 
-  describe('TEST', () => {
-    it('Shoud return true', () => {
+
+describe("TEST", function() {
+  describe("Return true", function() {
+    it("Should return true", function() {
       return true;
     });
   });
 });
-
-
