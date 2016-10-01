f861d50643d11626e724c06865cc450c19544ed8
Corwin Crownover <corwin.crownover@gmail.com>
2016-09-30 19:29:09 -0700

    Add logic for creating new UserDoc entry for new shared user

--- server/resources/controllers/userDocumentController.js
+++ server/resources/controllers/userDocumentController.js
@@ -4,20 +4,10 @@ var User = require('../../../db/Models/User');
 var Document = require('../../../db/Models/Document');
 var Promise = require('bluebird');
 
-/// HELPERS ///
-// PROBLEM: not returnnig user object as desired --> how to return value out of these query promises?
-// var getUser = function(userId) {
-//   console.log('getUser userId:', userId);
-//   User.findOne({where: {id: userId}})
-//     .then(function(user) {
-//       console.log('inside then user:', user.dataValues);
-//       return user.dataValues;
-//     })
-// }
 
-
-// Use case: on doc create
+// USE CASE: Immediately after doc create
 // After document creation OR when user opens doc, send request to server w/ req that includes userId & docId
+// NOTE: THE ABOVE ^ IS BASICALLY THE SAME SCENARIO, CAN COMBINE THEM INTO ONE?: ON DOCUMENT LOAD: 1ST RUN CHECK IF NEW USER, THEN CREATE ENTRY, THEN GET SHARED USERS
 exports.createUserDoc = function(req, res, next) {
   var userId = 2;
   var docId = req.body.docId;
@@ -33,15 +23,18 @@ exports.createUserDoc = function(req, res, next) {
     })
 };
 
-// Use case: on NavBar load, get shared users
+// USE CASE: on TextComponent OR NavBar load, get shared users
 // note: may want to modularize to be 1. getUserDocs, 2. Get sharedUsers
 // 1. get UserDoc entries
 // 2. Get userIds for each entry
 // 3. get userobject for each userId
 // 4. modify objects before sending back in the response
 
+// TODO: FE connection needs to pass in docId & current userId
+
 exports.getSharedUsers = function(req, res, next) {
   var docId = req.query.docId;
+  var curUserId = req.query.userId;
 
   UserDoc.findAll({where: {documentId: docId}})
     .then(function(userDocs) {
@@ -49,14 +42,32 @@ exports.getSharedUsers = function(req, res, next) {
         return userDoc.userId;
       });
 
+      // If current user did not already have an entry, render a chat head and create new UserDoc entry
+      if (sharedUserIds.indexOf(curUserId) === -1) {
+        sharedUserIds.push(curUserId);
+
+        User.findOne({where: {id: curUserId}})
+          .then(function(user) {
+            Document.find({where: {id: docId}})
+              .then(function(document) {
+                user.addDocument(document);
+              })
+          })
+          .then(function() {
+            return sharedUserIds;
+          })
+      }
+
       return sharedUserIds;
     })
     .then(function(userIds) {
+      // For each userId, get the entire user object
       return Promise.map(userIds, function(userId) {
         return User.findOne({where: {id: userId}});
       });
     })
     .then(function(array) {
+      // Create a filtered version of the user object that doesn't include sensitive / irrelevant info, e.g. password
       var filteredUsers = array.map(function(user) {
         var filteredUser = {};
         filteredUser.id = user.id;
