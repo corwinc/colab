053182cda067ee42769fb64a8e5077261de8317a
Surender <ilovehackreactor@gmail.com>
2016-10-04 15:55:53 -0700

    added password to user schema

--- client/rootReducer.jsx
+++ client/rootReducer.jsx
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './auth/reducers/authReducer.jsx';
+// import Comment from './comment/reducers/commentReducer.jsx';
 
 const rootReducer = combineReducers({
-	user : AuthReducer,  	
+	user : AuthReducer,  
+	// comment : Comment,
+	// text : Text,	
 });
 
 export default rootReducer;
