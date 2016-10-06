7fcef4e3bb98011fcfe558685e062d4aa4652eed
Surender <ilovehackreactor@gmail.com>
2016-10-05 21:17:35 -0700

    able to fix bug and only a unique user can go to the home page

--- client/index.jsx
+++ client/index.jsx
@@ -7,21 +7,21 @@ import reducers from './rootReducer.jsx';
 import routes from './app.routes.jsx';
 import promise from 'redux-promise';
 
-/*
-//if you want to use logger() to see all the states get updates 
-//into the browser console then these three lines of code.
-  //@Redux-logger
+
+// //if you want to use logger() to see all the states get updates 
+// //into the browser console then these three lines of code.
+//   //@Redux-logger
   
-  import createLogger from 'redux-logger';
+//   import createLogger from 'redux-logger';
 
 
 
-  const logger = createLogger();
+//   const logger = createLogger();
 
-  const createStoreWithMiddleware = applyMiddleware(thunk, promise, logger)(createStore);
+//   const createStoreWithMiddleware = applyMiddleware(thunk, promise, logger)(createStore);
   
-  //@
-*/
+//   //@
+
 
 //if you want to use redux devtool then use these 4 lines of code below:-
 
--- client/rootReducer.jsx
+++ client/rootReducer.jsx
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
-// import AuthReducer from './auth/reducers/authReducer.jsx';
+import AuthReducer from './auth/reducers/authReducer.jsx';
 import flashMessages from './auth/reducers/flashMessage.jsx';
 // import Comment from './comment/reducers/commentReducer.jsx';
 
 const rootReducer = combineReducers({
 	flashMessages: flashMessages,
-	//user : AuthReducer,  
+	user : AuthReducer,  
 	// comment : Comment,
 	// text : Text,	
 });
