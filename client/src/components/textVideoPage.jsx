49af1394d22eab538d5b2f145c89b8bf1e8751b9
Corwin Crownover <corwin.crownover@gmail.com>
2016-10-10 15:58:44 -0700

    Fix merge conflicts round 2

--- client/src/components/textVideoPage.jsx
+++ client/src/components/textVideoPage.jsx
@@ -6,7 +6,6 @@ import Chat from './chat.jsx';
 import NavBar from './navbar.jsx';
 import FlashMessagesList from '../../auth/components/flash/flashMessagesList.jsx';
 import CommentArea from '../../comments/components/CommentArea.jsx';
-// import CommentArea from './commentArea.jsx'; // PRE-REDUX HOOKUP
 
 // /* COMPONENT WITHOUT CHAT */
 export default class TextVideoPage extends React.Component {
@@ -27,20 +26,8 @@ export default class TextVideoPage extends React.Component {
 
     this.getSharedUsers = this.getSharedUsers.bind(this);
     this.getInitials = this.getInitials.bind(this);
-    this.getComments = this.getComments.bind(this);
     this.setSelectionLoc = this.setSelectionLoc.bind(this);
-    this.handleCommentInput = this.handleCommentInput.bind(this);
-    this.postEntry = this.postEntry.bind(this);
-    this.cancelEntry = this.cancelEntry.bind(this);
-    this.handleCommentClick = this.handleCommentClick.bind(this);
   };
-
-  componentWillMount () {
-    console.log('componentWillMount');
-    this.getComments();
-    // render comments from this.state.comments
-    // => for each comment, create a jsx element and attach it to DOM
-  }
 
   getSharedUsers (docId, userId) {
     // send request to server
@@ -80,81 +67,8 @@ export default class TextVideoPage extends React.Component {
     return firstInit + lastInit;
   }
 
-  //////// COMMENT METHODS /////////
-  getComments () {
-    // send request to server
-    // on success: setState w/ comments
-    // componentDidMount: render comments
-    console.log('inside getComments');
-    $.ajax({
-      method: 'GET',
-      url: '/comments',
-      dataType: 'json',
-      data: {docId: this.state.curDoc},
-      success: (data) => {
-        console.log('Success getting comments!:', data);
-        this.setState({comments: data}, () => {
-          console.log('state comments:', this.state.comments);
-        });  
-      },
-      error: (err) => {
-        console.log('error getting comments:', err);
-      }
-    })
-  }
-
   setSelectionLoc (loc) {
     this.setState({selectionLoc: loc});
-  }
-
-  handleCommentInput (e) {
-    console.log('inside handleCommentInput');
-    e.preventDefault();
-    this.setState({commentInput: e.target.value}, () => {
-      if (this.state.commentInput !== '') {
-        console.log('the input has value!:', this.state.commentInput);
-        this.setState({commentEntryHeight: 70, activeCommentStatus: true});
-      } else {
-        console.log('the input DOESNOT have value');
-        this.setState({commentEntryHeight: 50, activeCommentStatus: false});
-      }
-    });
-  }
-
-  postEntry () {
-    // send POST req to db w/ input data
-    var comment = {
-      text: this.state.commentInput,
-      block: 1,
-      user: this.state.curUser,
-      location: this.state.selectionLoc,
-      document: this.state.curDoc
-    }
-
-    $.ajax({
-      method: 'POST',
-      url: '/comments',
-      data: comment,
-      success: (data) => {
-        console.log('Success posting comment!:', data);
-        this.setState({selectionLoc: null});
-        this.getComments();
-      },
-      error: (err) => {
-        console.log('error posting entry:', err);
-      }
-    })
-    // on success: remove comment entry and show rendered unfocused comment
-  }
-
-  cancelEntry () {
-    // remove DOM ==> create activeCommentEntry state?
-    this.setState({commentInput: '', activeCommentStatus: false, commentEntryHeight: 50, selectionLoc: null});
-  }
-
-  handleCommentClick () {
-    this.setState({savedCommentFocus: true});
-    // => increase height, add border, add resolve link (delete)
   }
 
   render() {
@@ -198,16 +112,3 @@ export default class TextVideoPage extends React.Component {
 //     );
 //   }
 // }
-
-
-// <CommentArea 
-//   comments={this.state.comments}
-//   selectionLoc={this.state.selectionLoc} 
-//   handleCommentInput={this.handleCommentInput}
-//   commentInput={this.state.commentInput}
-//   commentEntryHeight={this.state.commentEntryHeight}
-//   activeCommentStatus={this.state.activeCommentStatus}
-//   postEntry={this.postEntry}
-//   cancelEntry={this.cancelEntry}
-//   handleCommentClick={this.handleCommentClick}
-//   savedCommentFocus={this.state.savedCommentFocus} />
