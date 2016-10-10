ccd4097424b0c94e907d1affeb41b865f3f86f8e
Vincent Barilla <vbarilla@gmail.com>
2016-10-09 22:20:32 -0700

    Delete placeholding comment snippets

--- client/src/components/callAlertIncoming.jsx
+++ client/src/components/callAlertIncoming.jsx
@@ -23,27 +23,3 @@ const IncomingCallAlert = ({username, acceptOnclick, rejectOnclick}) => {
 };
 
 export default IncomingCallAlert;
-
-//   $('#acceptIcon').unbind().on('click', function(){
-//     context.pcs[pcKey].createAnswer(successCallback, errorCallback);
-//   });
-//   $('#rejectIcon').unbind().on('click', function(){
-//     signalingChannel.emit('disconnect call', JSON.stringify({"pcKey": pcKey}));
-//   });
-
-//     <div className="call-alerts-incoming">
-//       <div className="call-incoming-notifications">
-//         <img id="seeOptionsIcon" className="call-icon" src="public/images/callwaiting.svg"></img>
-//         <span className="call-message"> Incoming call... </span>
-//       </div>
-//       <div className="call-incoming-options">
-//         <div id="acceptIcon">
-//           <img className="call-mini-icon" src="public/images/call.png"></img>
-//           Accept
-//         </div>
-//         <div id="rejectIcon">
-//           <img className="call-mini-icon" src="public/images/rejectcall.png"></img>
-//           Reject
-//         </div>
-//       </div>
-//     </div>
