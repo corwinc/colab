c19ca3631e8d6063248bf01b4fe751e213ed08b0
Brandon Tiqui <btiqui@gmail.com>
2016-10-11 12:14:40 -0700

    Add additional toolbar options

--- client/src/components/textEditor.jsx
+++ client/src/components/textEditor.jsx
@@ -35,13 +35,30 @@ class TextEditor extends React.Component {
     this.props.dispatch(editor.setLink(sharelinkId));
 
 
+    var toolbarOptions = [
+      ['bold', 'italic', 'underline', 'strike', 'image'],        // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      [{ 'direction': 'rtl' }],                         // text direction
+
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+      ['image']
+
+      ['clean']                                         // remove formatting button
+    ];
+
     var quill = new Quill('#editor', {
       modules: {
-        toolbar: [
-          [{header: [1, 2, false] }],
-          ['bold', 'italic', 'underline'],
-          ['image', 'code-block']
-        ]
+        toolbar: toolbarOptions
       },
       theme: 'snow'
     });
