4a9bcda21bbd6399f1e3165b0e16d82127ba6e83
Corwin Crownover <corwin.crownover@gmail.com>
2016-10-13 15:41:30 -0700

    Add documentation, cleanup navbar

--- client/src/actions/navbarActions.jsx
+++ client/src/actions/navbarActions.jsx
@@ -1,10 +1,3 @@
-export function setUserId(userId) {
-  return {
-    type: 'SET_USER_ID',
-    userId
-  }
-}
-
 export function SetCurSharedUsers(users) {
   return {
     type: 'SET_CUR_SHARED_USERS',
--- client/src/reducers/navbarReducer.jsx
+++ client/src/reducers/navbarReducer.jsx
@@ -1,14 +1,9 @@
 const initialState = {
-  userId: null,
   curSharedUsers: []
 }
 
 export default function (state=initialState, action) {
   switch (action.type) {
-    case 'SET_USER_ID':
-      return Object.assign({}, state, {
-        userId: action.userId
-      })
     case 'SET_CUR_SHARED_USERS':
       return Object.assign({}, state, {
         curSharedUsers: action.users
